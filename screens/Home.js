5d4307aa4cd0797578bc58bad98cdecad030fe6d
bijoya <bijoya10101@gmail.com>
2022-04-15 12:20:07 +0530

    PRO-C143-Reference-Code

--- screens/Home.js
+++ screens/Home.js
@@ -5,42 +5,96 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
-  ImageBackground
+  ImageBackground,
 } from "react-native";
 import { Icon } from "react-native-elements";
 import { RFValue } from "react-native-responsive-fontsize";
 import axios from "axios";
-import Star from 'react-native-star-view';
+import Star from "react-native-star-view";
 
 export default class HomeScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
+  constructor() {
+    super();
+    this.state = {
       movieDetails: {},
-      ngrok_url:""
-    }
+      ngrok_url: "https://6cd0-2405-201-8008-e095-a5b9-7f0b-7ea8-a76c.ngrok.io",
+    };
+  }
+
+  componentDidMount() {
+    this.getMovie();
   }
 
   /*define getmovie(), likedMovie(), dislikedMovie() ,notWatched() functions here*/
 
+  getMovie = () => {
+    const url = this.state.ngrok_url + "/movies";
+    axios
+      .get(url)
+      .then((response) => {
+        this.setState({ movieDetails: response.data.data });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  likedMovie = () => {
+    const url = this.state.ngrok_url + "/like";
+    axios
+      .get(url)
+      .then((response) => {
+        this.getMovie();
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  dislikedMovie = () => {
+    const url = this.state.ngrok_url + "/dislike";
+    axios
+      .get(url)
+      .then((response) => {
+        this.getMovie();
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  notWatched = () => {
+    const url = this.state.ngrok_url + "/did_not_watch";
+    axios
+      .get(url)
+      .then((response) => {
+        this.getMovie();
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
   render() {
+    const { movieDetails } = this.state;
+    if (movieDetails.poster_link) {
+      const { poster_link, original_title, release_date, duration, rating } =
+        movieDetails;
+
       return (
         <View style={styles.container}>
           <ImageBackground
             source={require("../assets/bg.png")}
             style={{ flex: 1 }}
           >
-            <View
-              style={styles.headerContainer}
-            >
+            <View style={styles.headerContainer}>
               <Text style={styles.headerTitle}>Movie Recommendation</Text>
               <Icon
                 name="chevron-right"
                 type="feather"
                 color={"white"}
-                size= {RFValue(30)}
-                containerStyle={{position:"absolute",right:RFValue(5)}}
+                size={RFValue(30)}
+                containerStyle={{ position: "absolute", right: RFValue(5) }}
                 onPress={() => {
                   this.props.navigation.navigate("Movies");
                 }}
@@ -49,27 +103,55 @@ export default class HomeScreen extends Component {
 
             <View style={styles.subContainer}>
               <View style={styles.posterContainer}>
-                  {/*Add the component for poster image below*/}
-
+                {/*Add the component for poster image below*/}
+                <Image
+                  style={styles.posterImage}
+                  source={{ uri: poster_link }}
+                />
               </View>
-              <View style={{flex:0.15}}>
-                 {/*Add the components to show the movie name and other details ( release date & duration) below*/}
-
-                
+              <View style={{ flex: 0.15 }}>
+                {/*Add the components to show the movie name and 
+                other details ( release date & duration) below*/}
+                <View style={styles.detailsContainer}>
+                  <Text style={styles.title}>{original_title}</Text>
+                  <Text style={styles.subtitle}>
+                    {release_date.split("-")[0]} | {duration} mins
+                  </Text>
+                </View>
               </View>
               <View style={styles.ratingContainer}>
                 {/*Add the components to show rating of the movie below*/}
-                  
+                <Star score={rating} style={styles.starStyle} />
               </View>
               <View style={styles.iconButtonContainer}>
                 {/*Add the code for like, dislike and notWatched button below*/}
-                
+                <TouchableOpacity onPress={this.likedMovie}>
+                  <Image
+                    style={styles.iconImage}
+                    source={require("../assets/like.png")}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.dislikedMovie}>
+                  <Image
+                    style={styles.iconImage}
+                    source={require("../assets/dislike.png")}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.notWatched}>
+                  <Image
+                    style={styles.iconImage}
+                    source={require("../assets/didNotWatch.png")}
+                  />
+                </TouchableOpacity>
               </View>
             </View>
           </ImageBackground>
         </View>
       );
+    } else {
+      return null;
     }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -82,7 +164,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flexDirection: "row",
     justifyContent: "flex-end",
-    backgroundColor:"#182854"
+    backgroundColor: "#182854",
   },
   headerTitle: {
     color: "#fff",
@@ -90,14 +172,14 @@ const styles = StyleSheet.create({
     fontSize: RFValue(18),
     fontFamily: "monospace",
     textAlign: "center",
-    flex: 1
+    flex: 1,
   },
   subContainer: {
     flex: 0.9,
   },
   posterContainer: {
     flex: 0.65,
-    marginBottom:RFValue(10),
+    marginBottom: RFValue(10),
     justifyContent: "center",
     alignItems: "center",
   },
@@ -115,8 +197,8 @@ const styles = StyleSheet.create({
     borderRadius: RFValue(10),
     marginHorizontal: RFValue(10),
     padding: RFValue(10),
-    borderColor:"#182854",
-    borderWidth:RFValue(2)
+    borderColor: "#182854",
+    borderWidth: RFValue(2),
   },
   title: {
     fontSize: RFValue(15),
@@ -134,7 +216,7 @@ const styles = StyleSheet.create({
   },
   ratingContainer: {
     flex: 0.1,
-    alignItems:"center"
+    alignItems: "center",
   },
   overview: {
     fontSize: RFValue(13),
@@ -155,5 +237,5 @@ const styles = StyleSheet.create({
   starStyle: {
     width: RFValue(200),
     height: RFValue(40),
-  }
+  },
 });
